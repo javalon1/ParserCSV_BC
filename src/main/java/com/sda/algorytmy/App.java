7940c9df48f52d7e1dbb9ece88e1aff85925a713
Bartlomiej Cyba <bartlomiejcyba@gmail.com>
2019-05-26 15:57:25 +0100

    final

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,35 @@
 package com.sda.algorytmy;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static Path file = Paths.get("file_tmp.csv");
+
+    public static void main(String[] args) {
+
+        try {
+            byte[] lines2 = Files.readAllBytes(file);
+            List<String> lines = Files.readAllLines(file);
+
+            System.out.println(lines.get(1));
+
+            String[] l = lines.get(1).split( ",");
+            System.out.println(l[22]);
+
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
